ae1858c05029daedf0b67bc2670163e1a51355c8
pan93412 <pan93412@gmail.com>
2018-12-22 23:22:38 +0800

    [SNAPSHOT] Add documentations.
    TAG: [NOT-TESTED][USELESS][SNAPSHOT][NOT-READY-FOR-RELEASE]

---
+++ docs/Menu[en_US].md
@@ -0,0 +1,24 @@
+# Telegram Bot Framework Menu
+## Notes
+### Q: What is the mean of `[]` after every filename?
+A: The language.
+
+- zh_TW: Chinese (Traditional) Documentations (The native language of author own, more complete than another.)
+- en_US: English (US) Documentations (The second language of author own, might has some wrong grammers.)
+
+### Q: When I found typo/documentation omission, how do I to report it?
+A: Open a Pull Request to send improved documentation to authors directly,
+   or open a Issues to let authors know that.
+   
+(!) When you submit omission/typo by Issues, the priority will be lower that PR.
+
+## Set up your bot
+- SetupBot: Documentations about set up bot.
+
+## Build modules and the related basic functions.
+- Modules: Documentations about modules.
+  - Module_Basic.md: 模組編寫入門
+  - Basic_Functions: 基礎函式
+    - ForwardMessage: 轉寄訊息函式
+    - SendMedia: 傳送媒體函式
+    - SendMessage: 傳送訊息函式
--- tgbot.go
+++ tgbot.go
@@ -33,7 +33,7 @@ type ModuleInfo struct {
 }
 
 // 模組基本介面：使用以下的基礎介面來架構出自己的機器人模組吧！
-// 
+//
 // (!) 製作模組需要一定的 Go 能力。但預設已內建多項模組可供使用。
 //
 // The module basic interface: Use the below interface to build your bot module!
@@ -45,40 +45,38 @@ type BasicModule interface {
     // Settings(): 設定介面。將會呼叫此函數來設定機器人。
     // Settings(): Settings interface: We will call the function to set your bot.
     Settings(Bot)
-    
+
     // Handler(): 接收訊息的函式。將會將內容丟進去這個函式。您只須負責解析。
     Handler(ChatData)
-    
+
     // Information(): 回傳這模組的資訊。
     Information() ModuleInfo
 }
 
-// 基本函式：傳送訊息的函式
+// 基本函式：傳送訊息函式
 //
 // BASIC FUNCTION: A function which can send message.
 //
 // 請參考文件 Please refer the documentation:
 // https://core.telegram.org/bots/api#sendmessage
-//
 func SendMessage(chat_id int, message string, parse_mode string, disable_web_page_preview bool, disable_notification bool, reply_to_message_id int) {}
 
-// 基本函式：轉傳訊息的函式
+// 基本函式：轉傳訊息函式
 //
 // BASIC FUNCTION: A function which can forward message.
 //
 // 請參考文件 Please refer the documentation:
 // https://core.telegram.org/bots/api#forwardmessage
-//
 func ForwardMessage(chat_id int, from_chat_id int, disable_notification bool, message_id int) {}
 
-// 基本函式：傳送媒體的函式。
+// 基本函式：傳送媒體函式
+//
+// (!) 因為官方提供之媒體方法之多，因此我們僅提供這些方法共同的
+//     功能，因此功能無法如此齊全。
+//     但您仍能自己實現自己的媒體函式。
 //
 // BASIC FUNCTION: A function which can send media.
 //
-// (!) 因為官方提供之媒體 method 之多，因此我們僅提供這些 method 共同的
-//     功能，因此功能無法如此齊全。
-//     但您仍能自己實現自己的媒體函式。
-// 
 // (!) Because there are so many media method provided by official,
 //     we just provided the common feature of these methods.
 //     But you still can implement your media functions.
@@ -90,11 +88,17 @@ func ForwardMessage(chat_id int, from_chat_id int, disable_notification bool, me
 // 請傳入您想要的功能進 `method` 引數。
 //
 // 傳送圖片 Send Photos: photo
+//
 // 傳送音訊 Send Audios: audio
+//
 // 傳送影片 Send Videos: video
+//
 // 傳送文件 Send Documents: document
 //
 // url = 文件中的 photo, audio, video, document... 參數
 //
 // url = The photo, audio, video, document parameter in documentation.
-func SendMedia(chat_id int, url string, caption string, disable_notification bool, reply_to_message_id int) {}
+func SendMedia(method string, chat_id int, url string, caption string, disable_notification bool, reply_to_message_id int) {}
+
+// 基本函式：URL GET 工具
+// 使用方法：[連結] [參數]
